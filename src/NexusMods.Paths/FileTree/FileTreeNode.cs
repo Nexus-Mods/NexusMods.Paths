cf7d4627e84639abc7e76cbcebfa345f3593d674
AL <26797547+Al12rs@users.noreply.github.com>
2023-08-21 13:18:01 +0200

    Added initial versions of FileTree abstractions

---
+++ src/NexusMods.Paths/FileTree/FileTreeNode.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace NexusMods.Paths.FileTree;
+
+/// <summary>
+/// Represents a generic tree of files with some associated value.
+/// </summary>
+/// <typeparam name="TPath">The path type used in the tree</typeparam>
+/// <typeparam name="TValue"></typeparam>
+[PublicAPI]
+public class FileTreeNode<TPath, TValue> : IFileTree<FileTreeNode<TPath, TValue>, TPath>
+    where TPath : struct, IPath<TPath>, IEquatable<TPath>
+{
+    private readonly bool _isFile;
+
+    private readonly Dictionary<RelativePath, FileTreeNode<TPath, TValue>> _children;
+    private FileTreeNode<TPath, TValue>? _parent;
+
+    /// <summary>
+    /// Constructs a new <see cref="FileTreeNode{TPath,TValue}"/> with the given path, name, parent and value.
+    /// NOTE: If the value is null, this node is assumed to be a directory, a file otherwise.
+    /// </summary>
+    /// <param name="path">The complete path for the node with respect to the root of the tree</param>
+    /// <param name="name">The file name for the node</param>
+    /// <param name="value">The associated value to be stored along a file entry. Should be null for directories.</param>
+    public FileTreeNode(TPath path, RelativePath name, TValue? value)
+    {
+        Path = path;
+        Name = name;
+        _isFile = value != null;
+        Value = value;
+        _children = new Dictionary<RelativePath, FileTreeNode<TPath, TValue>>();
+    }
+
+    /// <inheritdoc />
+    public TPath Path { get; }
+
+    /// <inheritdoc />
+    public RelativePath Name { get; }
+
+    /// <inheritdoc />
+    public bool IsFile => _isFile;
+
+    /// <inheritdoc />
+    public bool IsDirectory => !_isFile;
+
+    /// <inheritdoc />
+    public bool HasParent => _parent != null;
+
+    /// <inheritdoc />
+    public bool IsTreeRoot => !HasParent;
+
+    /// <summary>
+    /// A value associated with a File entry.
+    /// Is null for directories.
+    /// </summary>
+    public TValue? Value { get; }
+
+    /// <inheritdoc />
+    public IDictionary<RelativePath, FileTreeNode<TPath, TValue>> Children => _children;
+
+    /// <inheritdoc />
+    public FileTreeNode<TPath, TValue> Parent
+    {
+        get
+        {
+            if (_parent == null)
+            {
+                throw new InvalidOperationException("Root node has no parent");
+            }
+
+            return _parent;
+        }
+    }
+
+    /// <inheritdoc />
+    public FileTreeNode<TPath, TValue> Root
+    {
+        get
+        {
+            var current = this;
+            while (current.HasParent)
+            {
+                current = current.Parent;
+            }
+
+            return current;
+        }
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<FileTreeNode<TPath, TValue>> GetSiblings()
+    {
+        if (IsTreeRoot) return Enumerable.Empty<FileTreeNode<TPath, TValue>>();
+
+        return Parent.Children.Values.Where(x => x != this);
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<FileTreeNode<TPath, TValue>> GetAllFileDescendants()
+    {
+        if (IsFile) return Enumerable.Empty<FileTreeNode<TPath, TValue>>();
+
+        return Children.Values.SelectMany(x => x.GetAllFileDescendants());
+    }
+
+    /// <summary>
+    /// Adds a collection of nodes as children to this node.
+    /// Sets the parent of the children to this node.
+    /// </summary>
+    /// <param name="children">The collection of nodes to be added</param>
+    public void AddChildren(IEnumerable<FileTreeNode<TPath, TValue>> children)
+    {
+        foreach (var child in children)
+        {
+            child._parent = this;
+            AddChild(child);
+        }
+    }
+
+    /// <summary>
+    /// Adds a node as a child to this node.
+    /// Sets the parent of the child to this node.
+    /// </summary>
+    /// <param name="child">The node to be added</param>
+    public void AddChild(FileTreeNode<TPath, TValue> child)
+    {
+        child._parent = this;
+        _children.Add(child.Name, child);
+    }
+
+    /// <summary>
+    /// Creates a tree structure from a collection of file entries.
+    ///
+    /// NOTE: If the file paths are rooted, they all need to be sharing the same root component.
+    /// If the paths are not rooted, they are assumed to be relative to the same unknown root.
+    /// </summary>
+    /// <param name="files">A collection of unique file entries in the form of TPath,TValue pairs.</param>
+    /// <returns>The root node of the generated tree.</returns>
+    public static FileTreeNode<TPath, TValue>? CreateTree(IEnumerable<KeyValuePair<TPath, TValue>> files)
+    {
+        if (!files.Any()) return null;
+
+        var fileArray = files.ToArray();
+        var rootComponent = fileArray.First().Key.GetRootComponent;
+
+        // If paths are rooted, we assume all the passed paths share the same root
+        // If paths are not rooted, we assume they are all relative to the same unknown root (RelativePath.Empty)
+        var rootNode = new FileTreeNode<TPath, TValue>(rootComponent, RelativePath.Empty, default);
+
+        rootNode.PopulateTree(fileArray);
+
+        return rootNode;
+    }
+
+    /// <summary>
+    /// Populates the tree with the given collection of file entries.
+    /// The current node is assumed to be the root of the tree.
+    /// All file paths are assumed to be relative to the root of the tree.
+    /// </summary>
+    /// <param name="files">A collection of unique files in the form of TPath,TValue pairs.</param>
+    /// <exception cref="InvalidOperationException">If there are duplicate file entries.</exception>
+    protected void PopulateTree(IEnumerable<KeyValuePair<TPath, TValue>> files)
+    {
+        foreach (var fileEntry in files)
+        {
+            var parentNode = this;
+            var currentFullPath = fileEntry.Key;
+
+            var parentPaths = currentFullPath.GetAllParents().Reverse().ToArray();
+
+            // traverse the path from root to leaf and add missing nodes
+            for (var i = 0; i < parentPaths.Length; i++)
+            {
+                var subPath = parentPaths[i];
+                var subPathName = subPath.Name;
+
+                if (parentNode.Children.TryGetValue(subPathName, out var existing))
+                {
+                    // if we are at the last path, this is the file
+                    if (i == parentPaths.Length - 1)
+                    {
+                        throw new InvalidOperationException($"Duplicate path found for file: {subPath}");
+                    }
+
+                    parentNode = existing;
+                    continue;
+                }
+
+                // if we are at the last path, this is the file
+                var value = i == parentPaths.Length - 1 ? fileEntry.Value : default;
+
+                var node = new FileTreeNode<TPath, TValue>(subPath, subPathName, value);
+
+                parentNode.AddChild(node);
+
+                parentNode = node;
+            }
+        }
+    }
+}
---
+++ src/NexusMods.Paths/FileTree/IFileTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace NexusMods.Paths.FileTree;
+
+/// <summary>
+/// Represents a generic tree of files.
+/// </summary>
+/// <typeparam name="TFileTree">Allows implementations to return concrete types</typeparam>
+/// <typeparam name="TPath">The path type being used in the tree</typeparam>
+[PublicAPI]
+public interface IFileTree<TFileTree, TPath> where TFileTree : IFileTree<TFileTree, TPath>
+    where TPath : struct, IPath<TPath>, IEquatable<TPath>
+{
+    /// <summary>
+    /// The complete path of the node with respect to the root of the tree.
+    /// </summary>
+    public TPath Path { get; }
+
+    /// <summary>
+    /// The file name for this node.
+    /// </summary>
+    public RelativePath Name { get; }
+
+    /// <summary>
+    /// Returns true if node is assumed to be a file.
+    /// </summary>
+    public bool IsFile { get; }
+
+    /// <summary>
+    /// Returns true if node is assumed to be a directory.
+    /// </summary>
+    public bool IsDirectory { get; }
+
+    /// <summary>
+    /// Returns true if node is the root of the tree.
+    /// </summary>
+    public bool IsTreeRoot { get; }
+
+    /// <summary>
+    /// Returns tre if node has a parent.
+    /// </summary>
+    public bool HasParent { get; }
+
+    /// <summary>
+    /// A Dictionary containing all the children of this node, both files and directories.
+    /// The key is the <see cref="Name"/> of the child.
+    /// </summary>
+    public IDictionary<RelativePath, TFileTree> Children { get; }
+
+    /// <summary>
+    /// Returns the parent node if it exists, throws InvalidOperationException otherwise.
+    /// </summary>
+    public TFileTree Parent { get; }
+
+    /// <summary>
+    /// Returns the root node of the tree.
+    /// </summary>
+    public TFileTree Root { get; }
+
+    /// <summary>
+    /// A collection of all sibling nodes, excluding this one.
+    /// Returns an empty collection if this node is the root.
+    /// </summary>
+    public IEnumerable<TFileTree> GetSiblings();
+
+    /// <summary>
+    /// A collection of all File nodes that descend from this one.
+    /// Returns an empty collection if this node is a file.
+    /// </summary>
+    public IEnumerable<TFileTree> GetAllFileDescendants();
+}
