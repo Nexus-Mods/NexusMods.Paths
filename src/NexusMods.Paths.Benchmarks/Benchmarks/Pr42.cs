c038a3cb16707f34a2d70d1a677f1da2f27427eb
Sewer56 <sewer56lol@googlemail.com>
2024-03-13 10:56:18 +0000

    Added: Some sample benches to satisfy a curious soul.

---
+++ src/NexusMods.Paths.Benchmarks/Benchmarks/Pr42.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using NexusMods.Paths.Benchmarks.Interfaces;
+using NexusMods.Paths.Utilities;
+
+namespace NexusMods.Paths.Benchmarks.Benchmarks;
+
+[BenchmarkInfo("PR42", "Comparison for PR #42")]
+[MemoryDiagnoser]
+public class Pr42 : IBenchmark
+{
+    [Params(@"short", @"a/very/long/path/indeed/to/see/how/it/performs")]
+    public string Path1 { get; set; } = null!;
+
+    [Params("short", "a/very/long/path/indeed/to/see/how/it/performs")]
+    public string Path2 { get; set; } = null!;
+
+    private readonly IOSInformation _osInformation;
+
+    public Pr42()
+    {
+        _osInformation = OSInformation.Shared;
+    }
+
+    [Benchmark]
+    public string Current()
+    {
+        return PathHelpers.JoinParts(Path1, Path2, _osInformation);
+    }
+
+    [Benchmark]
+    public string OldMethod()
+    {
+        return JoinParts(Path1, Path2, _osInformation);
+    }
+
+    [SkipLocalsInit] // original method didn't have it, but it's still good for comparison.
+    private static string JoinParts(ReadOnlySpan<char> left, ReadOnlySpan<char> right, IOSInformation os)
+    {
+        var spanLength = PathHelpers.GetExactJoinedPartLength(left, right);
+        var buffer = spanLength > 512
+            ? GC.AllocateUninitializedArray<char>(spanLength)
+            : stackalloc char[spanLength];
+
+        var count = PathHelpers.JoinParts(buffer, left, right, os);
+        if (count == 0) return string.Empty;
+        return buffer.ToString();
+    }
+}
