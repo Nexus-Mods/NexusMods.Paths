4aafb221ee6afab82f3ce757a5b8f8eb03628adb
erri120 <erri120@fossmailer.de>
2025-03-20 12:13:15 +0100

    Make RelativePath constructor internal
    https://github.com/Nexus-Mods/NexusMods.App/pull/2905 has showed that a
    public constructor of `RelativePath` can result in unforseen issues. The
    entire point of `RelativePath` and `AbsolutePath` is to provide a
    consistent abstraction over file paths. To achieve this consistency,
    paths have to be sanitized, otherwise many assumptions made in code
    aren't valid anymore.
    
    Safety goes out the window with a public constructor that doesn't
    sanitized. I've added a `CreateUnsafe` method for the few cases where
    you want to skip sanitization because the input is already sanitized.

--- src/NexusMods.Paths/Extensions/StringExtensions.cs
+++ src/NexusMods.Paths/Extensions/StringExtensions.cs
@@ -1,12 +1,17 @@
+using System;
+using JetBrains.Annotations;
+
 namespace NexusMods.Paths.Extensions;
 
 /// <summary>
 /// Path related extensions tied to strings.
 /// </summary>
+[PublicAPI]
 public static class StringExtensions
 {
     /// <summary>
     /// Converts an existing path represented as a string to a <see cref="RelativePath"/>.
     /// </summary>
-    public static RelativePath ToRelativePath(this string s) => new(s);
+    [Obsolete($"Use implicit conversion or {nameof(RelativePath.FromUnsanitizedInput)}")]
+    public static RelativePath ToRelativePath(this string s) => s;
 }
